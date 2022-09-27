9d4ed626ea352e0e30b720c9bba4ca22ede098c1
Roman Pupko <romanpupko1994@gmail.com>
2022-09-27 20:06:53 +0300

    Add input system

--- Assets/Scripts/Infrastructure/Services/InputService.cs
+++ Assets/Scripts/Infrastructure/Services/InputService.cs
@@ -1,92 +1,68 @@
 using Infrastructure.Services.Input;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Lean.Common;
-using UnityEngine.EventSystems;
 
 public class InputService : MonoBehaviour, IInputService
 {
-    public Camera Camera;
-    public bool Rotate;
-    protected Plane Plane;
+    private Vector3 _touchStart, _touchSecond;
+    private ParentedCamera _cameraParent;
+    private Camera _camera;
+    private float groundZ = 0;
+    private float _startingDistance;
+    private int _touchCount = 1;
+    private float _sensibility = 0.3f;
 
     private void Awake()
     {
-        if (Camera == null)
-            Camera = Camera.main;
+        _cameraParent = FindObjectOfType<ParentedCamera>();
+        _camera = Camera.main;
     }
-
-    private void Update()
+    void Update()
     {
+        _touchCount = Input.touchCount;
 
-        //Update Plane
-        if (Input.touchCount >= 1)
-            Plane.SetNormalAndPosition(transform.up, transform.position);
-
-        var Delta1 = Vector3.zero;
-        var Delta2 = Vector3.zero;
-
-        //Scroll
-        if (Input.touchCount >= 1)
+        if (Input.GetMouseButtonDown(0))
         {
-            Delta1 = PlanePositionDelta(Input.GetTouch(0));
-            if (Input.GetTouch(0).phase == TouchPhase.Moved)
-                Camera.transform.Translate(Delta1, Space.World);
+            _touchStart = GetWorldPosition(groundZ);
         }
 
-        //Pinch
-        if (Input.touchCount >= 2)
+        if (Input.GetMouseButton(0))
         {
-            var pos1 = PlanePosition(Input.GetTouch(0).position);
-            var pos2 = PlanePosition(Input.GetTouch(1).position);
-            var pos1b = PlanePosition(Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition);
-            var pos2b = PlanePosition(Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition);
-
-            //calc zoom
-            var zoom = Vector3.Distance(pos1, pos2) /
-                       Vector3.Distance(pos1b, pos2b);
-
-            //edge case
-            if (zoom == 0 || zoom > 10)
-                return;
-
-            //Move cam amount the mid ray
-            Camera.transform.position = Vector3.LerpUnclamped(pos1, Camera.transform.position, 1 / zoom);
-
-            if (Rotate && pos2b != pos2)
-                Camera.transform.RotateAround(pos1, Plane.normal, Vector3.SignedAngle(pos2 - pos1, pos2b - pos1b, Plane.normal));
+            if (_touchCount < 2)
+            {
+                Vector3 direction = _touchStart - GetWorldPosition(groundZ);
+                _cameraParent.transform.position += direction;
+            }                     
         }
 
+        if (_touchCount >= 2)
+        {
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+            Vector2 touchZeroPrevPos = touch0.position - touch0.deltaPosition;
+            Vector2 touchOnePrevPos = touch1.position - touch1.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touch0.position - touch1.position).magnitude;
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            _cameraParent.transform.position += new Vector3(_cameraParent.transform.position.x, _cameraParent.transform.position.y + deltaMagnitudeDiff * _sensibility, _cameraParent.transform.position.z);
+        }
     }
 
-    protected Vector3 PlanePositionDelta(Touch touch)
+    private Vector3 GetWorldPosition(float z)
     {
-        //not moved
-        if (touch.phase != TouchPhase.Moved)
-            return Vector3.zero;
-
-        //delta
-        var rayBefore = Camera.ScreenPointToRay(touch.position - touch.deltaPosition);
-        var rayNow = Camera.ScreenPointToRay(touch.position);
-        if (Plane.Raycast(rayBefore, out var enterBefore) && Plane.Raycast(rayNow, out var enterNow))
-            return rayBefore.GetPoint(enterBefore) - rayNow.GetPoint(enterNow);
-
-        //not on plane
-        return Vector3.zero;
+        Ray mousePos = _camera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, new Vector3(0, 0, z));
+        float distance;
+        ground.Raycast(mousePos, out distance);
+        return mousePos.GetPoint(distance);
     }
 
-    protected Vector3 PlanePosition(Vector2 screenPos)
+    private float GetDistanceBetweenTouches()
     {
-        //position
-        var rayNow = Camera.ScreenPointToRay(screenPos);
-        if (Plane.Raycast(rayNow, out var enterNow))
-            return rayNow.GetPoint(enterNow);
-
-        return Vector3.zero;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(transform.position, transform.position + transform.up);
+        return Vector3.Distance(_touchStart, _touchSecond);
     }
 
     public Vector3 GetMovementDirection()
